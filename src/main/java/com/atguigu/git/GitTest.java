6de9497bbf08e0492c26e8e045a525fc080ff8c1
shiguanghuimou <50981417+shiguanghuimou@users.noreply.github.com>
2021-06-13 16:20:23 +0800

    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello master test");
         System.out.println("Hello hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
